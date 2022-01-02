345e8b9db33c361de0fd5e55c424c58a03142f7d
Evgenii Lebedev <ptyz2000@gmail.com>
2022-01-02 20:51:38 +0300

    frontend

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from uuid import uuid4
 
 
 class User(models.Model):
-    username = models.CharField(max_length=64)
+    uname = models.CharField(max_length=64)
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
-    email = models.EmailField(unique=True)
+    e_mail = models.EmailField(unique=True)
     UUID = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'firstname', 'lastname', 'email']
+        fields = ['url', 'uname', 'firstname', 'lastname', 'e_mail']
