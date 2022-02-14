7d2d3291c13ebe88599abebb9f3b0bc45182181e
Evgenii Lebedev <ptyz2000@gmail.com>
2022-02-14 12:19:12 +0300

    user side filtering

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,48 +1,52 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+class ProjectList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: props.projects
+        }
+    }
 
-const ProjectItem = ({project, deleteProject}) => {
-    // noinspection JSUnresolvedVariable
-    return (<tr>
-        <td>
-            {project.name}
-        </td>
-        <td>
-            <a href={project.repo}>{project.repo}</a>
-        </td>
-        <td>
-            {project.authors.map((item, i) => <option key={i}>{item}</option>)}
-        </td>
-        <td>
-            <button onClick={() => deleteProject(project.id)} type='button'>Delete</button>
-        </td>
-    </tr>)
-};
+    filterProjects(event) {
+        event.preventDefault();
+        this.setState({
+            projects: this.props.projects.filter(obj => {
+                return obj.name.startsWith(event.target[0].value)
+            })
+        })
+    }
 
-const ProjectList = ({projects, deleteProject}) => {
-    return (<div>
-        <table>
-            <thead>
-            <tr>
-                <th>
-                    name
-                </th>
-                <th>
-                    repo link
-                </th>
-                <th>
-                    authors
-                </th>
-                <th/>
-            </tr>
-            </thead>
-            <tbody>
-            {projects.map((project, i) => <ProjectItem project={project} deleteProject={deleteProject} key={i}/>)}
-            </tbody>
-        </table>
-        <Link to={'/projects/create'}>Create</Link>
-    </div>)
-};
+    render() {
+        return <div>
+            <form onSubmit={event => this.filterProjects(event)}>
+                <input type='text' name='filter'/>
+                <input type='submit' value='Filter'/>
+            </form>
+            <table>
+                <thead>
+                <tr>
+                    <th>name</th>
+                    <th>repo link</th>
+                    <th>authors</th>
+                    <th/>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.projects.map((project, i) => <tr key={i}>
+                    <td>{project.name}</td>
+                    <td><a href={project.repo}>{project.repo}</a></td>
+                    <td>{project.authors.map((item, i) => <option key={i}>{item}</option>)}</td>
+                    <td>
+                        <button onClick={() => this.props.deleteProject(project.id)} type='button'>Delete</button>
+                    </td>
+                </tr>)}
+                </tbody>
+            </table>
+            <Link to={'/projects/create'}>Create</Link>
+        </div>
+    }
+}
 
 export default ProjectList
